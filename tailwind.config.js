6fdb5fa542df2ec1c657dd1328fc6bbc56e48818
mudeesha <mudeeshatharindudilshan@gmail.com>
2025-06-16 07:52:20 +0530

    installed shadcn

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '15px',
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '960px',
+      'xl': '1200px',
+    },
+    // fontFamily: {
+    //   primary: "var(--font-jetbrainsMono)",
+    // },
+    extend: {
+      fontFamily: {
+        primary: "var(--font-jetbrainsMono)",
+      },
+    },
+  },
+  plugins: [],
+}
+
