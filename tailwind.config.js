b0b5d6ec82f845bd5176705b0c6e1bebca005143
mudeesha <mudeeshatharindudilshan@gmail.com>
2025-06-19 08:34:54 +0530

    config tailwind configsin css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,12 +15,17 @@ module.exports = {
       'lg': '960px',
       'xl': '1200px',
     },
-    // fontFamily: {
-    //   primary: "var(--font-jetbrainsMono)",
-    // },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
+    },
     extend: {
-      fontFamily: {
-        primary: "var(--font-jetbrainsMono)",
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
+        
       },
     },
   },
