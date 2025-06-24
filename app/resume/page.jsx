9c85fe14d3be009855602b76cc9442924fd11f97
mudeesha <mudeeshatharindudilshan@gmail.com>
2025-06-25 03:11:31 +0530

    redume section: created experiance section

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { Description } from "@radix-ui/react-dialog";
-import { FaHtml5, Facss3, FaJs, FaReact, FaFigma, FaNodejs } from "react-icons/fa";
+import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma } from "react-icons/fa";
+import { DiNodejsSmall } from "react-icons/di";
 import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
 
 const about = {
   title: 'About me',
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium similique laborum dolore id iste enim labore perspiciatis. Dicta, molestias possimus asperiores officiis at, consectetur culpa nobis amet dolores, magnam enim.",
   info: [
-    {
-      fieldName: "Name",
-      fieldValue: "Mudeesha Tharindu"
-    },
     {
       fieldName: "Name",
       fieldValue: "Mudeesha Tharindu"
@@ -37,7 +34,7 @@ const about = {
       fieldValue: "Available"
     },
     {
-      fieldName: "Freelance",
+      fieldName: "Languages",
       fieldValue: "English, Sinhala"
     },
   ]
@@ -50,29 +47,153 @@ const experience = {
   items: [
     {
       company: "Rightmo web solutions (pvt) LTD.",
-      Description: "Associate Software Engineer",
-      Duration: "2024 - 2025"
+      position: "Associate Software Engineer",
+      duration: "2024 - 2025"
+    },
+    {
+      company: "ABC web solutions (pvt) LTD.",
+      position: "Abc Engineer",
+      duration: "2024 - 2021"
+    },
+    {
+      company: "ABC web solutions (pvt) LTD.",
+      position: "Abc Engineer",
+      duration: "2024 - 2021"
+    },
+    {
+      company: "ABC web solutions (pvt) LTD.",
+      position: "Abc Engineer",
+      duration: "2024 - 2021"
     },
   ]
 };
 
 const education = {
   icon: '/assets/resume/cap.svg',
-  title: "My experience",
+  title: "My education",
   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium similique laborum dolore id iste enim labore perspiciatis. Dicta, molestias possimus asperiores officiis at, consectetur culpa nobis amet dolores, magnam enim.",
   items: [
     {
-      company: "Rightmo web solutions (pvt) LTD.",
-      Description: "Associate Software Engineer",
-      Duration: "2024 - 2025"
+      institution: "Online Course Platform",
+      Description: "Full Stack  Web Development Bootcamp",
+      Duration: "2023"
+    },
+    {
+      institution: "Codecadmy",
+      Description: "Backend",
+      Duration: "2022"
+    },
+    {
+      institution: "SpringBoot Acedamy",
+      Description: "JAVA",
+      Duration: "2024"
+    },
+    {
+      institution: "University of Ruhuna",
+      Description: "Batchler of Information Communication Technology.",
+      Duration: "2018-2023"
     },
   ]
 };
 
+const skills = {
+  tittle: "My Skills",
+  description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, nesciunt sunt perspiciatis expedita unde repellendus illum adipisci enim vero saepe nisi magnam, mollitia accusantium voluptas, et possimus. Obcaecati, eligendi quidem.",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "html 5",
+    },
+    {
+      icon: <FaCss3 />,
+      name: "css 3",
+    },
+    {
+      icon: <FaJs />,
+      name: "javascript",
+    },
+    {
+      icon: <FaReact />,
+      name: "react.js",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "next.js",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "tailwind.css",
+    },
+    {
+      icon: <DiNodejsSmall />,
+      name: "node.js",
+    },
+  ],
+};
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
 const Resume = () => {
   return (
-    <div>resume page</div>
-  )
-}
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: "easeIn"},
+    }}
+    className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+    >
+      <div className="container mx-auto">
+        <Tabs defaultValue="experience" className="flex flex-col xl:flex-row gap-[60px]">
+          
+          <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
+            <TabsTrigger value="experience" >Experience</TabsTrigger>
+            <TabsTrigger value="education">Education</TabsTrigger>
+            <TabsTrigger value="skills">Skills</TabsTrigger>
+            <TabsTrigger value="about">About me</TabsTrigger>
+          </TabsList>
+
+          <div className="min-h-[70vh] w-full">
+            <TabsContent value="experience" className="w-full">
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{experience.description}</p>
+                <ScrollArea className="h-[400px] overflow-y-auto">
+                  <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
+                    {experience.items.map((item, index) => (
+                      <li
+                        key={index}
+                        className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
+                      >
+                        <span className="text-accent">{item.duration}</span>
+                        <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">
+                          {item.position}
+                        </h3>
+                        <div className="flex items-center gap-3">
+                          <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                          <p className="text-white/60">{item.company}</p>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollArea>
+
+              </div>
+            </TabsContent>
+
+            <TabsContent value="education" className="w-full">
+              education
+            </TabsContent>
+            <TabsContent value="skills" className="w-full">
+              skills
+            </TabsContent>
+            <TabsContent value="about" className="w-full">
+              about
+            </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+  </motion.div>
+  );
+};
 
 export default Resume;
