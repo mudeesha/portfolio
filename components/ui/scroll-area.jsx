e1d26fadcc201544c102c85895de16d8bce8fed8
mudeesha <mudeeshatharindudilshan@gmail.com>
2025-06-26 02:14:58 +0530

    comopleated resume section

--- components/ui/scroll-area.jsx
+++ components/ui/scroll-area.jsx
@@ -43,7 +43,7 @@ function ScrollBar({
       {...props}>
       <ScrollAreaPrimitive.ScrollAreaThumb
         data-slot="scroll-area-thumb"
-        className="bg-border relative flex-1 rounded-full" />
+        className="relative flex-1 rounded-full bg-accent" />
     </ScrollAreaPrimitive.ScrollAreaScrollbar>
   );
 }
